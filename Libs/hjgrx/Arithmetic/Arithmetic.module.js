75aeabdd6aa6ff8bbdbd15e09d1dbb89237b630a
Hegar Garcia <hegargarcia@gmail.com>
2018-05-28 21:20:58 -0500

    Adding Arithmetic Module

--- Libs/hjgrx/Arithmetic/Arithmetic.module.js
+++ Libs/hjgrx/Arithmetic/Arithmetic.module.js
@@ -1,6 +1,42 @@
 export function sum() {
     const values = Array.from(arguments);
     const valid = values.every((val) => !isNaN(val));
-    if (!valid) throw new Error("Valores de válidos");
+
+    if (!valid) {
+        throw new Error("Valores no válidos");
+    }
+
     return values.reduce((acc, curr) => acc + curr, 0);
+}
+
+export function subs() {
+    const values = Array.from(arguments);
+    const valid = values.every((val) => !isNaN(val));
+
+    if (!valid) {
+        throw new Error("Valores no válidos");
+    }
+
+    return values.reduce((acc, curr) => acc - curr);
+}
+
+export function mult() {
+    const values = Array.from(arguments);
+    const valid = values.every((val) => !isNaN(val));
+
+    if (!valid) {
+        throw new Error("Valores no válidos");
+    }
+
+    return values.reduce((acc, curr) => acc * curr);
+}
+
+export function div(quotient, dividend) {
+    const valid = !isNaN(quotient) && !isNaN(dividend) && dividend !== 0;
+
+    if (!valid) {
+        throw new Error("Valores no válidos");
+    }
+
+    return (quotient / dividend);
 }
