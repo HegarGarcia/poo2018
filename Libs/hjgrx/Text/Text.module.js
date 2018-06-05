b3345b16eecc115c8010219046c45674e9c35f5a
Hegar Garcia <hegargarcia@gmail.com>
2018-06-04 19:44:54 -0500

    Refactoring Text Module

--- Libs/hjgrx/Text/Text.module.js
+++ Libs/hjgrx/Text/Text.module.js
@@ -1,21 +1,32 @@
-export function splitWords(txt) {
-    validate(txt);
-    return split(txt, /\r*\n|\r|\s|[.,]/g);
-}
-
-export function splitSentences(txt) {
-    validate(txt);
-    return split(txt, /[.?!,;<>¿¿]\s*/g);
-}
-
-export function splitParagraph(txt) {
-    validate(txt);
-    return split(txt, /\n+|\r+/g);
-}
-
-const split = (txt, regexp) => txt.split(regexp).filter((val) => val);
-const validate = (txt) => {
-    if (typeof txt !== "string") {
-        throw new Error("Valores no válidos");
+export class TextSplitter {
+    constructor(selector) {
+        const elem = document.querySelector(selector);
+        if (elem === null) throw new Error("Elemento no encontrado");
     }
-};
+
+    splitWords() {
+        let txt = this.validate();
+        return split(txt, /\r*\n|\r|\s|[.,]/g);
+    }
+
+    splitSentences(txt) {
+        let txt = this.validate();
+        return split(txt, /[.?!,;<>¿¿]\s*/g);
+    }
+
+    splitParagraph(txt) {
+        let txt = this.validate();
+        return split(txt, /\n+|\r+/g);
+    }
+
+    split(regexp) {
+        let txt = this.this.validate();
+        return txt.split(regexp).filter((val) => val);
+    }
+
+    validate() {
+        const txt = elem.value;
+        if (typeof txt !== "string") throw new Error("Valores no válidos");
+        return txt;
+    };
+}
