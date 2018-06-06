78ca2d23ff2c6e94d57fc666cbfea2fbe81d2c69
Hegar Garcia <hegargarcia@gmail.com>
2018-06-05 19:24:12 -0500

    Completing Util Module

--- Libs/hjgrx/Util/Util.module.js
+++ Libs/hjgrx/Util/Util.module.js
@@ -11,5 +11,25 @@ export function isNumber(num) {
 }
 
 export function isArray(arr) {
-    return arr instanceof Array;
+    return Array.isArray(arr);
+}
+
+export function random() {
+    return Math.random();
+}
+
+export function randomAB(min = 0, max = 10) {
+    let sort = Number.trichotomy(min, max);
+
+    if (sort === 1) {
+        let c = max;
+        max = min;
+        min = c;
+    }
+
+    return sort === 0 ? 0 : Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function randomN(max) {
+    return Math.floor(Math.random() * (max + 1));
 }
